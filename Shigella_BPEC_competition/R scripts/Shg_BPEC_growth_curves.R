bc0ca8eed1fb7f57202e1ef442a252b45b5968f2
Lang Yao <langyao@cmail.carleton.ca>
2022-04-27 20:39:31 -0400

    all 4 BPECs

--- Shigella_BPEC_competition/R scripts/Shg_BPEC_growth_curves.R
+++ Shigella_BPEC_competition/R scripts/Shg_BPEC_growth_curves.R
@@ -1,8 +1,9 @@
 library(readxl)
-Growth_Curve_0_20h_Clean <- read_excel("Dropbox/Github/Bacteriocin-OLC-F-1905/Shigella_BPEC_competition/long_format_data/Growth_Curve_0-20h_Clean.xlsx", 
-                                       col_types = c("text", "text", "text", 
-                                                     "numeric", "numeric", "numeric", 
-                                                     "numeric", "numeric", "numeric"))
+Growth_Curve_0_20h_Clean <- read_excel("~/Dropbox/Github/Bacteriocin-OLC-F-1905/Shigella_BPEC_competition/long_format_data/Growth_Curve_0-20h_Clean.xlsx", 
+                                       sheet = "Sheet1", col_types = c("text", 
+                                                                       "text", "text", "numeric", "numeric", 
+                                                                       "numeric", "numeric", "numeric", 
+                                                                       "numeric"))
 
 library(tidyverse)
 
@@ -26,7 +27,7 @@ logCFU_3095 <- filter(logCFU_Growth_Curve, EC_ID %in% c('OLC1614', 'OLC3095'))
 
 library(ggplot2)
 
-ggplot(logCFU_0637,aes(x = Incubation_Time, y = logCFU_AVG,
+OLC0637 <- ggplot(logCFU_0637,aes(x = Incubation_Time, y = logCFU_AVG,
                        shape = EC_Description,
                        color = Count_Target)) +
   geom_point(size = 2) +
@@ -37,10 +38,90 @@ ggplot(logCFU_0637,aes(x = Incubation_Time, y = logCFU_AVG,
                 width = 0.25) +
   labs(x = "Incubation Time (h)", y = "log CFU/mL") +
   scale_x_continuous(breaks = c(0,1,2,3,4,5,6,7,20)) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 13, face = 'bold', color = 'black'),
-        axis.text = element_text(size = 10.5),
+  theme(legend.position = "right",
+        plot.title = element_text(size = 9, face = 'bold', color = 'black'),
+        axis.text = element_text(size = 10),
         # hjust 0.5 = in middle
         axis.text.x = element_text(angle = 0, hjust = 0.5),
-        axis.title = element_text(size = 13, face = 'bold'),
-        strip.text = element_text(size = 13, face = 'bold')) 
+        axis.title = element_text(size = 9, face = 'bold'),
+        strip.text = element_text(size = 9, face = 'bold')) +
+  guides(color = guide_legend(title = 'Target for colony counts'),
+         linetype = guide_legend(title = 'E. coli'),
+         shape = guide_legend(title = NULL))
+
+OLC1512 <- ggplot(logCFU_1512,aes(x = Incubation_Time, y = logCFU_AVG,
+                                  shape = EC_Description,
+                                  color = Count_Target)) +
+  geom_point(size = 2) +
+  geom_line(aes(linetype = EC_Description)) +
+  geom_errorbar(aes(ymin = logCFU_AVG - logCFU_SD,
+                    ymax = logCFU_AVG + logCFU_SD),
+                position = position_dodge(0),
+                width = 0.25) +
+  labs(x = "Incubation Time (h)", y = "log CFU/mL") +
+  scale_x_continuous(breaks = c(0,1,2,3,4,5,6,7,20)) +
+  theme(legend.position = "right",
+        plot.title = element_text(size = 9, face = 'bold', color = 'black'),
+        axis.text = element_text(size = 10),
+        # hjust 0.5 = in middle
+        axis.text.x = element_text(angle = 0, hjust = 0.5),
+        axis.title = element_text(size = 9, face = 'bold'),
+        strip.text = element_text(size = 9, face = 'bold')) +
+  guides(color = guide_legend(title = 'Target for colony counts'),
+         linetype = guide_legend(title = 'E. coli'),
+         shape = guide_legend(title = NULL))  
+
+OLC3094 <- ggplot(logCFU_3094,aes(x = Incubation_Time, y = logCFU_AVG,
+                                  shape = EC_Description,
+                                  color = Count_Target)) +
+  geom_point(size = 2) +
+  geom_line(aes(linetype = EC_Description)) +
+  geom_errorbar(aes(ymin = logCFU_AVG - logCFU_SD,
+                    ymax = logCFU_AVG + logCFU_SD),
+                position = position_dodge(0),
+                width = 0.25) +
+  labs(x = "Incubation Time (h)", y = "log CFU/mL") +
+  scale_x_continuous(breaks = c(0,1,2,3,4,5,6,7,20)) +
+  theme(legend.position = "right",
+        plot.title = element_text(size = 9, face = 'bold', color = 'black'),
+        axis.text = element_text(size = 10),
+        # hjust 0.5 = in middle
+        axis.text.x = element_text(angle = 0, hjust = 0.5),
+        axis.title = element_text(size = 9, face = 'bold'),
+        strip.text = element_text(size = 9, face = 'bold')) +
+  guides(color = guide_legend(title = 'Target for colony counts'),
+         linetype = guide_legend(title = 'E. coli'),
+         shape = guide_legend(title = NULL))  
+
+OLC3095 <- ggplot(logCFU_3095,aes(x = Incubation_Time, y = logCFU_AVG,
+                                  shape = EC_Description,
+                                  color = Count_Target)) +
+  geom_point(size = 2) +
+  geom_line(aes(linetype = EC_Description)) +
+  geom_errorbar(aes(ymin = logCFU_AVG - logCFU_SD,
+                    ymax = logCFU_AVG + logCFU_SD),
+                position = position_dodge(0),
+                width = 0.25) +
+  labs(x = "Incubation Time (h)", y = "log CFU/mL") +
+  scale_x_continuous(breaks = c(0,1,2,3,4,5,6,7,20)) +
+  theme(legend.position = "right",
+        plot.title = element_text(size = 9, face = 'bold', color = 'black'),
+        axis.text = element_text(size = 10),
+        # hjust 0.5 = in middle
+        axis.text.x = element_text(angle = 0, hjust = 0.5),
+        axis.title = element_text(size = 9, face = 'bold'),
+        strip.text = element_text(size = 9, face = 'bold')) +
+  guides(color = guide_legend(title = 'Target for colony counts'),
+         linetype = guide_legend(title = 'E. coli'),
+         shape = guide_legend(title = NULL))
+
+library(ggpubr)
+
+ggarrange(OLC0637, OLC1512, OLC3094, OLC3095,
+          labels = 'AUTO',
+          ncol = 2,
+          nrow = 2)
+
+
+
+
